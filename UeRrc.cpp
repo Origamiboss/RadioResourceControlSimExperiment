1abba8d34970ce13ef97ee15ca64535e702167ce
Origamiboss <vincentclimer@gmail.com>
2025-11-27 17:54:53 -0600

    a

--- UeRrc.cpp
+++ UeRrc.cpp
@@ -76,7 +76,7 @@ void UeRrc::sendRrcConnectionComplete() {
 
 void UeRrc::sendDummyData() {
     if (state == RrcState::RRC_CONNECTED) {
-        onst size_t dataSize = 1 * 1024 * 1024;  // 10 MB dummy packet
+        const size_t dataSize = 1 * 1024 * 1024;  // 10 MB dummy packet
         pdcp::PDcp::Bytes payload;
         payload.resize(dataSize);
 
