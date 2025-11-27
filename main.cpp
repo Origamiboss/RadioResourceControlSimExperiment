4474ea0638ad55957d8962ea0d9854ec4d673f96
Origamiboss <vincentclimer@gmail.com>
2025-11-27 15:33:52 -0600

    a

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ int main() {
                 sentComplete = true;
             }else{
                 ue.sendRrcConnectionRequest();
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
             }
 
         }
