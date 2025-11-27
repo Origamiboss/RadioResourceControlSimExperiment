4f72450044dad40a7213c338e630b3dd5124256a
Origamiboss <vincentclimer@gmail.com>
2025-11-27 15:34:38 -0600

    a

--- main.cpp
+++ main.cpp
@@ -54,11 +54,11 @@ int main() {
             }
 
         }
-        for(int i = 0; i < 1000; i++){
+        /*for(int i = 0; i < 1000; i++){
             ue.sendDummyData();
             std::this_thread::sleep_for(std::chrono::milliseconds(20));
             ue.checkForPackets();
-        }
+        }*/
         
 
         // After receiving the setup message
