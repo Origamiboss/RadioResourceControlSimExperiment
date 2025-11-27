2cc0fb5ebfd2598b0d30f4a787488aa3a2c28f98
Origamiboss <vincentclimer@gmail.com>
2025-11-27 10:19:24 -0600

    a

--- DistributedUnit.cpp
+++ DistributedUnit.cpp
@@ -52,15 +52,15 @@ void DistributedUnit::checkForCuPackets() {
 }
 void DistributedUnit::checkForUePackets() {
     if(f1cBuffer->empty()) return;
-    std::cout << "[DU] Waiting for CU packets...\n";
+    std::cout << "[DU] Waiting for UE packets...\n";
     auto optPacket = f1uBuffer->getPacket(); // this BLOCKS until packet arrives
     
     if (!optPacket) return;
-    std::cout << "[DU] CU packet received\n";
+    std::cout << "[DU] UE packet received\n";
 
     auto processed = proccessUpLink(*optPacket);
     if (processed.empty()) return;
-    std::cout << "[DU] Forwarding CU packet to UE\n";
+    std::cout << "[DU] Forwarding UE packet to CU\n";
     f2uBcffer->sendPacket(processed);
 
     
