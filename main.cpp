7bfdb0ad70ecafad66120f2974c178bf5a3f9cc1
Origamiboss <vincentclimer@gmail.com>
2025-11-27 10:16:08 -0600

    a

--- main.cpp
+++ main.cpp
@@ -46,6 +46,7 @@ int main() {
             ue.checkForPackets();
             for(int i = 0; i < 1000; i++){
                 ue.sendDummyData();
+                std::cout << "Sent Dummy Data Packet " << i+1 << "\n";
                 std::this_thread::sleep_for(std::chrono::milliseconds(20));
                 ue.checkForPackets();
             }
