053a43d959f29a994f6b81b3802d75aa132fc748
Origamiboss <vincentclimer@gmail.com>
2025-11-28 16:19:20 -0600

    a

--- Utils.cpp
+++ Utils.cpp
@@ -1,6 +1,7 @@
 // Utils.cpp (or a similar name)
 #include "Utils.hpp"
 #include <ctime>
+#include <chrono>
 
 std::string getCurrentTimestamp() {
     std::time_t now = std::time(nullptr);
@@ -8,8 +9,8 @@ std::string getCurrentTimestamp() {
     std::strftime(buf, sizeof(buf), "%Y-%m-%d %H:%M:%S", std::localtime(&now));
     return std::string(buf);
 }
-static inline long elapsedMs(const std::chrono::high_resolution_clock::time_point &start) {
-    using clock = std::chrono::high_resolution_clock;
+static inline long elapsedMs(const std::chrono::steady_clock::time_point &start) {
+    using clock = std::chrono::steady_clock;
     return std::chrono::duration_cast<std::chrono::milliseconds>(clock::now() - start).count();
 }
 
--- Utils.hpp
+++ Utils.hpp
@@ -3,7 +3,7 @@
 #define UTILS_HPP
 
 #include <string>
-
+#include<chrono>
 // Declaration of the function
 std::string getCurrentTimestamp();
 static inline long elapsedMs(const std::chrono::high_resolution_clock::time_point &start);
