229c3f31e6fb16784294730295032ae4bf8e561d
Origamiboss <vincentclimer@gmail.com>
2025-11-28 16:23:39 -0600

    a

--- Utils.hpp
+++ Utils.hpp
@@ -6,6 +6,6 @@
 #include<chrono>
 // Declaration of the function
 std::string getCurrentTimestamp();
-static inline long elapsedMs(const std::chrono::high_resolution_clock::time_point &start);
+static inline long elapsedMs(const std::chrono::steady_clock::time_point &start);
 #endif // UTILS_HPP
 
