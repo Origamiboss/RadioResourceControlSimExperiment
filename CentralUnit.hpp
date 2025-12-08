c450c363c60d823a0624125a724e832beae5ccc4
Origamiboss <vincentclimer@gmail.com>
2025-12-08 17:33:01 -0600

    fixed the hpp file as well

--- CentralUnit.hpp
+++ CentralUnit.hpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <fstream>
+#include <memory>
 #include "PcapLogger.hpp"
 #include "PDcp.hpp"
 #include "RrcState.hpp"
@@ -15,32 +16,36 @@ private:
     std::ofstream logFile;
     PcapLogger pcapLogger{"../Logs/ue_rrc.pcap"};
 
-    //Packet Queue
-    PacketBuffer* f1cBuffer; // To NetworkRrc
-    PacketBuffer* f1uBuffer; // To UE
-    PacketBuffer* theirBuffer;
+    // Packet queues
+    PacketBuffer* f1cBuffer; // DU -> CU (uplink)
+    PacketBuffer* f1uBuffer; // CU -> DU (downlink)
 
     int optionType;
     LatencyModel latency;
-    
+
+    // Delay model
     int computeFronthaulDelayUs(size_t sizeBytes);
     int computeProcessingDelayUs();
     int computeUuDelayUs();
     void applyLatency(int sizeBytes);
+
+    // New processing helpers
+    pdcp::PDcp::Bytes processUpLink(pdcp::PDcp::Bytes& raw);
+    pdcp::PDcp::Bytes processDownlink(pdcp::PDcp::Bytes& raw);
+
 public:
     CentralUnit(PacketBuffer* f1cBuffer, PacketBuffer* f1uBuffer, int optionType);
+
+    void changeOptionType(int newType);
 
     void checkForPackets();
 
     void sendRrcSetup();
-
-    void receiveRrcConnectionComplete();
-
     void sendRrcRelease();
-
     void sendDummyData();
 
     void receiveRrcConnectionRequest();
-    
-    [[maybe_unused]] RrcState getState() const;
+    void receiveRrcConnectionComplete();
+
+    [[maybe_unused]] RrcState getState() const { return state; }
 };
