f5f16ad6a56dac903d81a0444df76e462cb6c896
Origamiboss <vincentclimer@gmail.com>
2025-12-08 21:07:47 -0600

    a

--- Attacker.cpp
+++ Attacker.cpp
@@ -64,7 +64,7 @@ void Attacker::attackTargetBuffer() {
                 FuzzAttackStep();
                 runCounter++;
                 std::cout << "[Attacker] Fuzzing packet #" << runCounter << "\n";
-                std::this_thread::sleep_for(std::chrono::microseconds(50));
+                std::this_thread::sleep_for(std::chrono::microseconds(70));
                 break;
 
             case AttackMode::HYBRID:
